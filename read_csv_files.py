2162b2d6424d887469537eda6abb65dc1b1e74cf
Anthony081111 <ajh081111@gmail.com>
2024-01-06 11:23:49 -0500

    Confusion over read_csv_files.py.

--- read_csv_files.py
+++ read_csv_files.py
@@ -3,24 +3,33 @@ import pandas as pd
 
 # Read a csv file into a dataframe
 
-my_blocks_df = pd.read_csv("blocks_that_work_v2.csv")
+my_blocks_df = pd.read_csv("blocks_that_work_v3.csv")
+for i in range(20):
+    print(f"{my_blocks_df['block_number'][i], my_blocks_df['block_description'][i]}")
 
 
 def get_block_description(block_number):
     # Return the description of the block number
+    for items in my_blocks_df:
+        print(items)
+        if my_blocks_df[my_blocks_df["block_number"] == str(block_number)]:
+            print(items)
+    quit()
+
     block_data = my_blocks_df[my_blocks_df["block_number"] == str(block_number)]
-    desc = block_data["block_description"].values[0]
-    status = block_data["status"].values[0]
+    print(block_data)
+    desc = block_data['block_description'][block_number]
+    status = block_data['status'][block_number]
     return desc, status
 
 
 def change_detail(block_number, block_status):
     selected_row = my_blocks_df.loc[my_blocks_df["block_number"] == block_number]
-    my_blocks_df.loc[my_blocks_df["block_number"] == block_number, 'block_status'] = block_status
+    my_blocks_df.loc[my_blocks_df["block_number"] == block_number, 'status'] = block_status
 
 
 def save_csv():
-    my_blocks_df.to_csv("blocks_that_work_v2.csv", index=False)
+    my_blocks_df.to_csv("blocks_that_work_v3.csv", index=False)
 
 
 if __name__ == "__main__":
