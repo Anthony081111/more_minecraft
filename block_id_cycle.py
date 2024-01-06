96299036fe60071270b8720e9967791313b41401
Anthony081111 <ajh081111@gmail.com>
2024-01-06 10:04:44 -0500

    Attempting to fix gaps in blocks_that_work_v2.csv

--- block_id_cycle.py
+++ block_id_cycle.py
@@ -1,6 +1,7 @@
 """Creates a cycle of blocks starting at block id 0"""
 from mcpi.minecraft import Minecraft
 import WHERE_ARE_MY_STUPID_COORDINATES as whereCoord
+import read_csv_files
 
 mc = Minecraft.create()
 
@@ -12,12 +13,35 @@ mc.player.setTilePos(x, y, z)
 
 keep_going = True
 current_id = int(input("What is your starting id?"))
+original_id = current_id
 while keep_going:
+    blocks_that_work = []
     for i in range(0, 10):
         x = x+2
         mc.setBlock(x, y, z, 0)
         mc.setBlock(x, y, z, current_id)
+        invalid_block_id = True
+        while invalid_block_id:
+            try:
+                description, status = read_csv_files.get_block_description(current_id)
+                invalid_block_id = False
+            except:
+                current_id +=1
+        print(f"Id:  {current_id}   Description:  {description}   Status:  {status}")
+        blocks_that_work.append(current_id)
         current_id += 1
+    for item in blocks_that_work:
+        invalid_answer = True
+        while invalid_answer:
+            valid = input(f"Was id {item} valid?(y is valid, n needs research, ? does not exist)")
+            if valid == "y" or valid == "n" or valid == "?":
+                invalid_answer = False
+        if valid == "y":
+            read_csv_files.change_detail(item, "valid description")
+        elif valid == "n":
+            read_csv_files.change_detail(item, "needs research")
+        elif valid == "?":
+            read_csv_files.change_detail(item, "doesn't exist")
     stop = input("Do you want to keep going?")
     if stop == "n":
         keep_going = False
--- read_csv_files.py
+++ read_csv_files.py
@@ -3,14 +3,24 @@ import pandas as pd
 
 # Read a csv file into a dataframe
 
-my_blocks_df = pd.read_csv("blocks_that_work.csv")
+my_blocks_df = pd.read_csv("blocks_that_work_v2.csv")
 
 
 def get_block_description(block_number):
     # Return the description of the block number
-    block_data = my_blocks_df[my_blocks_df["Block_Number"] == str(block_number)]
-    desc = block_data["Block_Description"].values[0]
-    return desc
+    block_data = my_blocks_df[my_blocks_df["block_number"] == str(block_number)]
+    desc = block_data["block_description"].values[0]
+    status = block_data["status"].values[0]
+    return desc, status
+
+
+def change_detail(block_number, block_status):
+    selected_row = my_blocks_df.loc[my_blocks_df["block_number"] == block_number]
+    my_blocks_df.loc[my_blocks_df["block_number"] == block_number, 'block_status'] = block_status
+
+
+def save_csv():
+    my_blocks_df.to_csv("blocks_that_work_v2.csv", index=False)
 
 
 if __name__ == "__main__":
