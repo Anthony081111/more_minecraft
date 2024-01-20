bc6b5b9a038e8b71f993600bddc33641808d74fc
ellsberry <sellsberry@sbcglobal.net>
2024-01-20 10:54:09 -0500

    added minor changes

--- block_id_cycle.py
+++ block_id_cycle.py
@@ -18,21 +18,22 @@ while keep_going:
     blocks_that_work = []
     for i in range(0, 10):
         x = x+2
-        mc.setBlock(x, y, z, 0)
-        mc.setBlock(x, y, z, current_id)
+        mc.setBlock(x, y, z, 0)                                      # Replace what is at location with a block of air
+        mc.setBlock(x, y, z, current_id)                             # Place a block with block number = current_id
         invalid_block_id = True
         while invalid_block_id:
             try:
                 description, status = read_csv_files.get_block_description(current_id)
                 invalid_block_id = False
             except:
-                current_id +=1
-        print(f"Id:  {current_id}   Description:  {description}   Status:  {status}")
+                current_id += 1
+                invalid_block_id = False
+        print(f"block_id_cycle line 30:  Id:  {current_id}   Description:  {description}   Status:  {status}")
         blocks_that_work.append(current_id)
         current_id += 1
     for item in blocks_that_work:
         description, status = read_csv_files.get_block_description(current_id)
-        print("Line 34\n", blocks_that_work)
+        print("block_id_cycle line 36:  \n", blocks_that_work)
         invalid_answer = True
         while invalid_answer:
             valid = input(f"Was id {item} valid?(y is valid, n needs research, ? does not exist)")
@@ -41,6 +42,7 @@ while keep_going:
         if valid == "y":
             read_csv_files.change_detail(item, "valid description", description)
         elif valid == "n":
+            print(f"block_id_cycle line 45: block id = {item} description = {description}" )
             read_csv_files.change_detail(item, "needs research", description)
         elif valid == "?":
             read_csv_files.change_detail(item, "doesn't exist", description)
